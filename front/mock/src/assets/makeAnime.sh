a8256ed290db9db69907e74ab3aa00eb5ef22821
Kai Yamada <gpioblink@aol.com>
2019-04-29 16:11:30 +0900

    Generate Gif-animations

---
+++ front/mock/src/assets/makeAnime.sh
@@ -0,0 +1,24 @@
+convert -layers optimize -loop 0 -delay 40 dc/dc-corrected-*.gif dc/dc-corrected-anim.gif
+convert -layers optimize -loop 0 -delay 40 dc/dc-gameover-*.gif dc/dc-gameover-anim.gif
+convert -layers optimize -loop 0 -delay 40 dc/dc-incorrect-*.gif dc/dc-incorrected-anim.gif
+convert -layers optimize -loop 0 -delay 40 dc/dc-selected-*.gif dc/dc-selected-anim.gif
+convert -layers optimize -loop 0 -delay 40 dc/dc-thinking-*.gif dc/dc-thinking-anim.gif
+
+convert -layers optimize -loop 0 -delay 40 ps/ps-corrected-*.gif ps/ps-corrected-anim.gif
+convert -layers optimize -loop 0 -delay 40 ps/ps-gameover-*.gif ps/ps-gameover-anim.gif
+convert -layers optimize -loop 0 -delay 40 ps/ps-incorrected-*.gif ps/ps-incorrected-anim.gif
+convert -layers optimize -loop 0 -delay 40 ps/ps-selected-*.gif ps/ps-selected-anim.gif
+convert -layers optimize -loop 0 -delay 40 ps/ps-thinking-*.gif ps/ps-thinking-anim.gif
+
+convert -layers optimize -loop 0 -delay 40 xp/xp-corrected-*.gif xp/xp-corrected-anim.gif
+convert -layers optimize -loop 0 -delay 40 xp/xp-gameover.gif xp/xp-gameover-anim.gif
+convert -layers optimize -loop 0 -delay 40 xp/xp-incorrected-*.gif xp/xp-incorrected-anim.gif
+convert -layers optimize -loop 0 -delay 40 xp/xp-selected-*.gif xp/xp-selected-anim.gif
+convert -layers optimize -loop 0 -delay 40 xp/xp-thinking-*.gif xp/xp-thinking-anim.gif
+
+convert -layers optimize -loop 0 -delay 40 fc/fc-corrected-*.gif fc/fc-corrected-anim.gif
+convert -layers optimize -loop 0 -delay 40 fc/fc-gameover-*.gif fc/fc-gameover-anim.gif
+convert -layers optimize -loop 0 -delay 40 fc/fc-incorrected-*.gif fc/fc-incorrected-anim.gif
+convert -layers optimize -loop 0 -delay 40 fc/fc-selected-*.gif fc/fc-selected-anim.gif
+convert -layers optimize -loop 0 -delay 40 fc/fc-thinking-*.gif fc/fc-thinking-anim.gif
+
